279cf201b0dfe175f12d1e771964e280c55c6e4b
NikitaIT <NIKITA2008-101@yandex.ru>
2016-03-08 16:46:37 +0300

    1079 - Максимум

---
+++ CSharp_SimpleTasks/max/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            List<int> q = new List<int>();
+            int x;
+            while ((x = Convert.ToInt32(Console.ReadLine()))!=0)
+            {
+                q.Add(x);
+            }
+            List<int> QList = new List<int>();
+            for (int i = 0; i < q.Count; i++)
+            {
+                QList.Clear();
+                QList.Add(0);
+                QList.Add(1);
+                for (int j = 2; j <= q[i]; j++)
+                {
+                    if (j%2 == 0) QList.Add(QList[j/2]);
+                    else
+                    {
+                        QList.Add(QList[(j - 1)/2 + 1] + QList[(j - 1)/2]);
+                    }
+                }
+                Console.WriteLine(QList.Max());
+            }
+            Console.ReadKey();
+        }
+    }
+}
