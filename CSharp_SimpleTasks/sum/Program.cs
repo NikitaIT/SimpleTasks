890a3c53626bce6799cbd5b50a7a6f653d813c32
NikitaIT <NIKITA2008-101@yandex.ru>
2016-03-08 13:53:02 +0300

    1068 - Сумма

---
+++ CSharp_SimpleTasks/sum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Convert.ToInt32(Console.ReadLine());
+            long sum=0;
+            for (var j = 1; j <= Math.Abs(x); j++)
+            {
+                sum += j;
+            }
+            Console.WriteLine(Math.Sign(x)*sum + ((Math.Sign(x)==-1)?1:0));
+        }
+    }
+}
