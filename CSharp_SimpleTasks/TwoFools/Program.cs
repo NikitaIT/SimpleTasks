99defae9e684153903a13c8aabf7310c15f733cb
NikitaIT <NIKITA2008-101@yandex.ru>
2016-03-08 12:12:43 +0300

    1409 - Два бандита

---
+++ CSharp_SimpleTasks/TwoFools/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoFools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] x = Console.ReadLine().Split(' ');
+            Console.WriteLine((10 - Convert.ToInt32(x[0])) + " " + (10 - Convert.ToInt32(x[1])));
+        }
+    }
+}
